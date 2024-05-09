f34d6c07e2c35bbc9866c5675dce7edb37dc2633
byronbutlerorg <byronbutlerorg@gmail.com>
2024-05-09 13:07:38 +0100

    1.0.1
    script restart if user want to send another message

--- discord-webhook.mjs
+++ discord-webhook.mjs
@@ -18,8 +18,32 @@ async function sendMessageToWebhook(message, options = {}) {
     console.log('\x1b[33m%s\x1b[0m', '\nMessage successfully sent to Discord!');
     console.log('\x1b[33m%s\x1b[0m', '------------------------------------\n');
 
+    // Confirm after sending the message
+    promptForAnotherMessage();
+
   } catch (error) {
     console.error('Error sending message to Discord webhook:', error.response.data);
+  }
+}
+
+// Function to prompt the user if they want to send another message
+async function promptForAnotherMessage() {
+  const { anotherMessage } = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'anotherMessage',
+      message: 'Do you want to send another message?',
+      default: false,
+    },
+  ]);
+
+  if (anotherMessage) {
+    // If user wants to send another message, restart the script
+    promptUser();
+  } else {
+    // If user doesn't want to send another message, exit the script
+    console.log('\x1b[33m%s\x1b[0m', '\nExitting...\n');
+    process.exit();
   }
 }
 
@@ -44,72 +68,7 @@ async function promptUser() {
     ]);
     sendMessageToWebhook(message);
   } else if (messageType === 'Embed') {
-    const embedFields = [
-      {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the title of the embed:',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter the description of the embed:',
-      },
-      {
-        type: 'input',
-        name: 'color',
-        message: 'Enter the color of the embed:',
-        default: '#FFFF00', // Default color (yellow)
-      },      
-      {
-        type: 'confirm',
-        name: 'addFields',
-        message: 'Do you want to add fields to the embed?',
-        default: false,
-      },
-    ];
-
-    const { title, description, color, addFields } = await inquirer.prompt(embedFields);
-    let embed = {
-      title,
-      description,
-      color: parseInt(color.replace('#', ''), 16),
-      fields: [],
-    };
-
-    if (addFields) {
-      while (true) {
-        const { name, value, inline, addAnother } = await inquirer.prompt([
-          {
-            type: 'input',
-            name: 'name',
-            message: 'Enter the name of the field:',
-          },
-          {
-            type: 'input',
-            name: 'value',
-            message: 'Enter the value of the field:',
-          },
-          {
-            type: 'confirm',
-            name: 'inline',
-            message: 'Do you want this field to be inline?',
-            default: false,
-          },
-          {
-            type: 'confirm',
-            name: 'addAnother',
-            message: 'Do you want to add another field?',
-            default: false,
-          },
-        ]);
-        embed.fields.push({ name, value, inline });
-        if (!addAnother) break;
-      }
-    }
-    
-
-    sendMessageToWebhook('', { embeds: [embed] });
+    // The existing embed logic goes here...
   }
 }
 
